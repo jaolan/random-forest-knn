388d1c3c2f58fbb3b776a2aacff07a7981c2e794
jaolan <43867679+jaolan@users.noreply.github.com>
2018-11-13 15:50:59 -0600

    Add files via upload
    -Implemented header stripping from given data_attribute file
    -Main now combines headers from data attribute file to training set in pandas

--- forest_knn.py
+++ forest_knn.py
@@ -3,6 +3,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
 
 #df = pd.read_table('/Users/superman/Downloads/train.nmv.txt', delimiter=" ", sep =" ")
 HEADER_FILE = "/Users/superman/Downloads/attr.txt"
@@ -13,11 +14,16 @@ def get_headers():
     with open(HEADER_FILE) as hf:
         headers = [line.rstrip('\n') for line in hf]
         hf.close
-        return headers\
+        return headers
 
 headers = get_headers()
 df = pd.read_table(TRAINING_FILE, delimiter=' ', names=headers)
-
+#weight=balanced since year col and others skew attribute balance
+random_forest = RandomForestClassifier(random_state=0, n_jobs=-1, class_weight="balanced")
 #print(headers)
 #df.iloc
+#TODO  drop last col till can figure out why headers dont match up
+df.drop(df.columns[[-1,]], axis=1, inplace=True)
 print(df)
+print(df.describe())
+print(df.plot())
